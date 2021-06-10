9579be4aa929a167a098acea52fb5ae477c65508
student <atul.kumbhar1985@gmail.com>
2021-06-10 15:19:23 +0000

    second try

--- src/main/java/simplilearn/com/AssessPratice1/AssessPratice1Application.java
+++ src/main/java/simplilearn/com/AssessPratice1/AssessPratice1Application.java
@@ -2,8 +2,12 @@ package simplilearn.com.AssessPratice1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class AssessPratice1Application {
 
 	public static void main(String[] args) {
